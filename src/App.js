59c60ded5764a365c30effc90a8eec35e2e67701
3ndrius <andrzejo1991@gmail.com>
2019-05-24 13:13:27 +0200

    Adding app component filter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
 import UsersList from './components/UsersList';
 
+const users = ['Mark', 'John', 'Jane', 'Karol', 'Martin', 'Tom'];
 class App extends Component {
 
   state = {
-    text: '',
-    users: ['Mark', 'John', 'Jane', 'Karol', 'Martin', 'Tom'],
+    filtered: users
     
+  }
+
+  filterUser = (e) => {
+    let text = e.target.value;
+    let filtered = users.filter(user=> {
+      return (
+        user.toLowerCase().includes(text.toLowerCase())
+      )
+    })
+    this.setState({
+      filtered
+    })
   }
 
 
   render() {
     return (
       <div className="App">
-        <input type="text"/>
+        <input type="search"  onChange={this.filterUser}/>
 
-        <UsersList users={this.state.users} />
+        <UsersList users={this.state.filtered} />
 
       </div>
     )
